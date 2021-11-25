9b1cd294b6cd6e80b54f5aa9126557f3843c5188
MorphicResonance <55984436+MorphicResonance@users.noreply.github.com>
2021-11-25 07:53:08 +0300

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Additional packages in the ISO
-PKGS="bzip2 cryptsetup curl dialog ddrescue gnupg2 git gptfdisk gzip efibootmgr lvm2 makepasswd mdadm mosh nano p7zip rsync wget emacs unzip xz zip usb-modeswitch ModemManager mobile-broadband-provider-info grub-x86_64-efi xrandr nyxt linux-firmware-network"
+PKGS="bzip2 cryptsetup curl dialog ddrescue gnupg2 git gptfdisk gzip efibootmgr lvm2 makepasswd mdadm mosh nano p7zip rsync wget emacs unzip xz zip usb-modeswitch ModemManager mobile-broadband-provider-info grub-x86_64-efi xrandr nyxt linux-firmware-network NetworkManager network-manager-applet ntp xterm void-repo-multilib xorg-minimal xorg-input-drivers xorg-video-drivers setxkbmap xauth vdpauinfo libvdpau-va-gl font-misc-misc terminus-font dejavu-fonts-ttf dbus-elogind-x11 dkms void-repo-nonfree dmenu"
 
 yes y | xbps-install -Su xbps
 yes y | xbps-install -Su
